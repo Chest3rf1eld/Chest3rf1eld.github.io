6b056a5cfa1e26126545702de8d200a69c4a4855
Nikita Chaturov <nikchester01@gmail.com>
2026-09-16 19:52:32 +0400

    Use JetBrains Mono for terminal typography

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import '@fontsource/inter/latin-400.css';
 import '@fontsource/inter/latin-700.css';
 import '@fontsource/inter/cyrillic-400.css';
 import '@fontsource/inter/cyrillic-700.css';
+import '@fontsource/jetbrains-mono/latin-400.css';
+import '@fontsource/jetbrains-mono/latin-700.css';
+import '@fontsource/jetbrains-mono/cyrillic-400.css';
+import '@fontsource/jetbrains-mono/cyrillic-700.css';
 import './styles.css';
 import { App } from './App';
 
