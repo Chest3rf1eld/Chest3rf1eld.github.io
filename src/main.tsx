df7829e67c38f10ea58f8e07e0b1163767ce3d85
Nikita Chaturov <nikchester01@gmail.com>
2026-09-16 19:42:02 +0400

    Use Inter for UI typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-700.css';
+import '@fontsource/inter/cyrillic-400.css';
+import '@fontsource/inter/cyrillic-700.css';
 import './styles.css';
 import { App } from './App';
 
